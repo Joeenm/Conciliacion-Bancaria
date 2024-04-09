2821d5a44b819b4d75ae69c41bae53f3e2f0bae7
RaulArica <140749417+RaulArica@users.noreply.github.com>
2024-04-08 21:19:34 -0500

    No funcional en index
    En este punto, el archivo anulacion sirve por si solo, pero no sirve en el index

--- Inicio/AnulacionCKS.php
+++ Inicio/AnulacionCKS.php
@@ -1,4 +1,5 @@
 <?php
+ini_set('display_errors', 1);
 // Datos de conexión a la base de datos
 $servername = "localhost";
 $username = "d52024";
@@ -6,6 +7,26 @@ $password = "12345";
 $database = "conciliación";
 // Crear conexión
 $conn = new mysqli($servername, $username, $password, $database);
+$numero_cheque = "";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Verificar si se ha enviado un número de cheque
+  if (isset($_POST['numero_cheque'])) {
+      $numero_cheque = $_POST['numero_cheque'];
+      $sql = "SELECT * FROM cheques WHERE numero_cheque = '$numero_cheque'";
+      $result = $conn->query($sql);
+      echo "<script>console.log('El cheque exite, el metodo post sirvio');</script>";
+      echo "<script>";
+      echo "document.getElementById('fecha').value = '$fecha';";
+      echo "document.getElementById('p-orden-a').value = '$beneficiario';";
+      echo "document.getElementById('monto').value = '$monto';";
+      echo "document.getElementById('descripcion').value = '$descripcion';";
+      echo "</script>";
+    }
+  else{
+    echo "<script>console.log('Error al ejecutar la consulta: " . $conn->error . "');</script>";
+  }
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -17,51 +38,40 @@ $conn = new mysqli($servername, $username, $password, $database);
     <script src="Validaciones.js" defer></script>
 </head>
 <body>
-
+<form method="post">
+  
 <div id="titulo">
   <h1>Anulación de Cheques</h1>
-
-
     <div class="contenedor-cheques">
-        <div id="contenido-cheques">
-            
+        <div id="contenido-cheques">            
           <div id="ck1">
             <div id="numero-cheque">
                 <h4>No. de Cheque</h4>
-                <input type="text" id="numero-cheque" onkeypress="return SoloNumeros(event)">
-            </div>
-            
+                <input type="text" id="numero-cheque" name="numero_cheque" value="<?php echo $numero_cheque;?>" onkeypress="return SoloNumeros(event)">
+            </div>           
             <div class="contenedor-botones">
-                <button class="boton">Buscar</button>
-              </div>
-          
+                <button type="submit" class="boton">Buscar</button>
+              </div>         
           </div>
-
           <div id="FCH">
             <div id="Fecha">
                 <h4>Fecha</h4>
-                <input type="date" id="Fecha" readonly>
+                <input type="date" id="fecha" readonly>
             </div>  
-          </div>
-
-        
+          </div>      
           <div id="ck2">
             <h4>Páguese a la orden de</h4>
             <div id="ck2-1">
               <input type="text" id="p-orden-a" readonly>
-            </div>
-        
+            </div>      
             <h4>La suma de</h4>
-            <input type="text" readonly>
-           
+            <input type="text" id="monto" readonly>          
           </div>
-
           <div id="ck3">
             <h4>Descripción de Gasto</h4>
-            <input type="text" readonly>
+            <input type="text" id="descripcion" readonly>
           </div>
-        </div>
-        
+        </div>        
         <div class="contenedor-detalles-Anulación">
           <div id="contenido-Detalle-Anulación">
             <div id="og-1">
@@ -72,12 +82,9 @@ $conn = new mysqli($servername, $username, $password, $database);
               <button class="boton">Anular</button>
             </div>
           </div>
-
         </div>
-
     </div>
-
 </div>
-
+</form>
 </body>
 </html>
